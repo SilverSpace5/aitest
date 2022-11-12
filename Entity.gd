0cd127ada757aef8580cbd348f6d7720c46131a4
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-11-12 19:04:49 +1030

    e

--- Entity.gd
+++ Entity.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 #HI
-var brain = AiNet.AINet.new(6, 3, 1, 5)
+var brain = AiNet.AINet.new(10, 3, 3, 5)
 var braincopy = []
 export (float) var speed = 50
 export (float) var rotateSpeed = 3
@@ -11,6 +11,7 @@ var time = 0
 var move = Vector2(0, 0)
 var longestTime = 0
 var see = 0
+var dying = 0
 
 func _ready():
 	get_parent().get_parent().entities += 1
@@ -29,10 +30,10 @@ func _process(delta):
 	energy -= 1
 	$Label.text = str(round(energy/maxEnergy*100)) + "%"
 	
-	var du = 500
-	var dd = 500
-	var dl = 500
-	var dr = 500
+	var du = 500 # Up Distance
+	var dd = 500 # Down Distance
+	var dl = 500 # Left Distance
+	var dr = 500 # Right Distance
 	
 	if $up.is_colliding():
 		du = position.distance_to($up.get_collision_point())*2
@@ -43,7 +44,32 @@ func _process(delta):
 	if $right.is_colliding():
 		dr = position.distance_to($right.get_collision_point())*2
 	
-	brain.setInput([du, dd, dl, dr, energy, 1])
+	$rightShow.visible = $right.is_colliding()
+	$rightShow.position.x = dr
+	$leftShow.visible = $left.is_colliding()
+	$leftShow.position.x = -dl
+	$upShow.visible = $up.is_colliding()
+	$upShow.position.y = -du
+	$downShow.visible = $down.is_colliding()
+	$downShow.position.y = dd
+	
+	var colliders = [0, 0, 0, 0]
+	
+	var raycasts = [$right, $left, $up, $down]
+	
+	for i in range(len(raycasts)-1):
+		var raycast = raycasts[i]
+		if raycast.is_colliding() and raycast.get_collider():
+			if "banana" in raycast.get_collider().name:
+				colliders[i] = 1
+			if "Entity" in raycast.get_collider().name:
+				colliders[i] = 2
+			if "StaticBody2D" in raycast.get_collider().name:
+				colliders[i] = 3
+			if "coconut" in raycast.get_collider().name:
+				colliders[i] = 4
+	
+	brain.setInput([du/500, colliders[0], dd/500, colliders[1], dl/500, colliders[2], dr/500, colliders[3], energy, 1])
 	brain.update()
 	var output = brain.output()
 	if output[0] > 0.5:
@@ -53,38 +79,33 @@ func _process(delta):
 	if output[2] > 0.5:
 		rotation_degrees -= rotateSpeed
 	
-	if energy <= 0:
-		get_parent().get_parent().entities -= 1
-		if time > get_parent().get_parent().longestTime:
-			get_parent().get_parent().longestTime = time
-			get_parent().get_parent().lastNet = brain.nodes
-		queue_free()
+	if energy <= 0 or dying > 0:
+		dying += delta
+		$AnimationPlayer.play("despawn")
+		if dying >= 0.5:
+			get_parent().get_parent().entities -= 1
+			if time > get_parent().get_parent().longestTime and time >= 10:
+				get_parent().get_parent().longestTime = time
+				get_parent().get_parent().lastNet = brain.nodes
+			queue_free()
 	
 	vel *= 0.9
-	var oldPos = position
-	move_and_slide(Vector2(vel, 0).rotated(rotation))
-	move = position-oldPos
+	if dying <= 0:
+		var oldPos = position
+		move_and_slide(Vector2(vel, 0).rotated(rotation))
+		move = position-oldPos
+	else:
+		$Label.text = ""
 	
 
 func _on_foodDetect_area_entered(area):
-	if area.name == "food":
+	if area.name == "banana":
 		energy += 37.5
 		area.get_parent().queue_free()
 		get_parent().get_parent().food -= 1
 		get_parent().get_parent().spawn(position-move*3, brain.nodes)
-
-func _on_food1Detect_area_entered(area):
-	if area.name == "food1":
-		energy += 75
+	if area.name == "coconut":
+		energy = maxEnergy
 		area.get_parent().queue_free()
 		get_parent().get_parent().food -= 1
 		get_parent().get_parent().spawn(position-move*3, brain.nodes)
-
-func _on_eyes_area_entered(area):
-	if area.name == "food":
-		see = 1
-#		print("",see)
-
-func _on_eyes_area_exited(area):
-	see = 0
-
--- Game.gd
+++ Game.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 var entities = 0
 var food = 0
+export (int) var cameraSpeed = 50
+export (int) var startEntities = 5
 export (int) var maxEntities = 10
 export (int) var maxFood = 25
 var timer = 0
 var foodTimer = 0
+var cocunutTimer = 0
 var lastNet = []
 var longestTime = 0
+var auto = false
 
 func spawn(pos=Vector2(0, 0), net=[]):
 	if entities < maxEntities:
@@ -19,6 +23,7 @@ func spawn(pos=Vector2(0, 0), net=[]):
 
 func _process(delta):
 	foodTimer += delta
+	cocunutTimer += delta
 	if foodTimer > 0:
 		foodTimer = 0
 		if food < maxFood:
@@ -26,8 +31,15 @@ func _process(delta):
 			var food = load("res://food.tscn").instance()
 			add_child(food)
 			food.position = Vector2(rand_range(-1500, 1500), rand_range(-1500, 1500))
+	if cocunutTimer > 0.05:
+		cocunutTimer = 0
+		if food < maxFood:
+			food += 1
+			var food = load("res://food1.tscn").instance()
+			add_child(food)
+			food.position = Vector2(rand_range(-1500, 1500), rand_range(-1500, 1500))
 	
-	if entities > 0:
+	if entities > 0 and auto:
 		$Camera2D.zoom = Vector2(0.5, 0.5)
 		var posXs = []
 		var posYs = []
@@ -45,8 +57,8 @@ func _process(delta):
 			if $Camera2D.position.y-node.position.y > yRange[1]:
 				yRange[1] = $Camera2D.position.y-node.position.y
 		var zoomAmount = (xRange[1]-xRange[0])/1024+(yRange[1]-yRange[0])/512
-		$Camera2D.zoom.x += (zoomAmount+1-$Camera2D.zoom.x)/10
-		$Camera2D.zoom.y += (zoomAmount+1-$Camera2D.zoom.y)/10
+		$Camera2D.zoom.x += ((zoomAmount+1)*1.5-$Camera2D.zoom.x)/10
+		$Camera2D.zoom.y += ((zoomAmount+1)*1.5-$Camera2D.zoom.y)/10
 		var posX = 0
 		var posY = 0
 		for value in posXs:
@@ -56,6 +68,19 @@ func _process(delta):
 		posX /= len(posXs)
 		posY /= len(posYs)
 		$Camera2D.position = Vector2(posX, posY)
+	else:
+		$Camera2D.zoom = Vector2(1, 1)
+		if Input.is_action_pressed("right"):
+			$Camera2D.position.x += cameraSpeed
+		if Input.is_action_pressed("left"):
+			$Camera2D.position.x -= cameraSpeed
+		if Input.is_action_pressed("up"):
+			$Camera2D.position.y -= cameraSpeed
+		if Input.is_action_pressed("down"):
+			$Camera2D.position.y += cameraSpeed
+	
+	if Input.is_action_just_pressed("auto"):
+		auto = not auto
 	
 #	timer += delta
 #	if timer > 0.5:
@@ -64,11 +89,11 @@ func _process(delta):
 #		spawn(Vector2(rand_range(50, 950), rand_range(50, 550)))
 	
 	if entities < 1:
-		for i in range(100):
+		for i in range(startEntities):
 			randomize()
 			spawn(Vector2(rand_range(50, 950), rand_range(50, 550)), lastNet)
 
 func _ready():
-	for i in range(100):
+	for i in range(startEntities):
 		randomize()
 		spawn(Vector2(rand_range(50, 950), rand_range(50, 550)), lastNet)
